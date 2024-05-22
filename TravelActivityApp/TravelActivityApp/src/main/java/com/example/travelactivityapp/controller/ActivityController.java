fe7e16c5227b81c74785a38555113c5d7fd687d0
Erica Sowder <esowder@indeed.com>
2024-05-22 16:39:51 -0500

    create controller classes and revise service classes crud and controller crud - also create AuthController and AuthService classes

--- TravelActivityApp/TravelActivityApp/src/main/java/com/example/travelactivityapp/controller/ActivityController.java
+++ TravelActivityApp/TravelActivityApp/src/main/java/com/example/travelactivityapp/controller/ActivityController.java
@@ -1,4 +1,50 @@
 package com.example.travelactivityapp.controller;
 
+import com.example.travelactivityapp.model.Activity;
+import com.example.travelactivityapp.service.ActivityService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@RestController
+@RequestMapping("/activities")
 public class ActivityController {
+    @Autowired
+    private ActivityService activityService;
+
+    @GetMapping
+    public ResponseEntity<List<Activity>> getAllActivities() {
+        return ResponseEntity.ok(activityService.getAllActivities());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Activity> getActivityById(@PathVariable Integer id) {
+        Optional<Activity> activity = activityService.getActivityById(id);
+        if (activity.isPresent()) {
+            return ResponseEntity.ok(activity.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Activity> createActivity(@RequestBody Activity activity) {
+        return ResponseEntity.ok(activityService.saveActivity(activity));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Activity> updateActivity(@PathVariable Integer id, @RequestBody Activity activityDetails) {
+        return ResponseEntity.ok(activityService.updateActivity(id, activityDetails));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteActivity(@PathVariable Integer id) {
+        activityService.deleteActivity(id);
+        return ResponseEntity.noContent().build();
+    }
 }
