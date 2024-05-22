f1b83622520ba4c1684d88f3891d07a464d4a987
Erica Sowder <esowder@indeed.com>
2024-05-22 14:31:32 -0500

    basic backend bootstrap completed sans postgres password

---
+++ TravelActivityApp/TravelActivityApp/src/main/java/com/example/travelactivityapp/repository/IProfileRepository.java
@@ -0,0 +1,4 @@
+package com.example.travelactivityapp.repository;
+
+public interface IProfileRepository {
+}
---
+++ TravelActivityApp/TravelActivityApp/src/test/java/com/example/travelactivityapp/TravelActivityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelactivityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelActivityAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
