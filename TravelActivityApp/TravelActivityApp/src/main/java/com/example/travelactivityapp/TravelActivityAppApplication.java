93b53a83a932be764286e81662fd31370295eaca
Erica Sowder <esowder@indeed.com>
2024-05-22 15:42:16 -0500

    revise app properties, pom, and main TravelActivityAppApplication to reflect connection to SQL DB

--- TravelActivityApp/TravelActivityApp/src/main/java/com/example/travelactivityapp/TravelActivityAppApplication.java
+++ TravelActivityApp/TravelActivityApp/src/main/java/com/example/travelactivityapp/TravelActivityAppApplication.java
@@ -2,8 +2,12 @@ package com.example.travelactivityapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.example.travelactivityapp.model.entity"}) // include this?
+@EnableJpaRepositories(basePackages = {"com.example.travelactivityapp.repository"}) //include this?
 public class TravelActivityAppApplication {
 
     public static void main(String[] args) {
